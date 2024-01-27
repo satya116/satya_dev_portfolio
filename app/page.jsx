d53147559f66e5ff1916c17d5c8b78eb9dd1f52a
satya <satya@extendtech.net>
2024-01-27 17:06:42 +0530

    init

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import HeroBanner from "./_components/HeroBanner";
+import Card from "./_components/Card";
+
+export default function Home() {
+  return (
+    <>
+    <HeroBanner />
+
+<div style={{display: "flex", justifyContent: "space-around", alignItems: "center"}}>
+<Card />
+<Card />
+<Card />
+<Card />
+
+</div>
+
+
+    </>
+  );
+}
